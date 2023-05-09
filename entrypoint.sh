e406159f5c8f4a874d3d4598701686f212990126
Sebastien Vermeille <sebastien.vermeille@sonarsource.com>
2023-05-09 08:20:57 +0200

    BUILD-2700 Adapt gh-action_dogfood_merge (replace set-output)

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,5 +21,5 @@ git octopus origin/dogfood/* origin/master
 # Push to the dogfood branch
 git diff -s --exit-code HEAD origin/$1 || git push origin +HEAD:$1
 
-echo ::set-output name=dogfood-branch::$1
-echo ::set-output name=sha1::$(git rev-parse HEAD)
+echo "name=dogfood-branch::$1" >> "$GITHUB_OUTPUT"
+echo "name=sha1::$(git rev-parse HEAD)" >> "$GITHUB_OUTPUT"
